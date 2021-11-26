863e112daa9bd32f650a3d1dcd74cdc4661b1f0a
Atul <atulpratapsingh67@gmail.com>
2021-11-26 23:56:21 +0530

    Updates

---
+++ precache-manifest.d20ac7526b74f9d303ccbc400b917c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c51397e3b2f77ddcc7c367e18afad58",
+    "url": "/ReactProject_1/index.html"
+  },
+  {
+    "revision": "c13463d2999dcf7d9317",
+    "url": "/ReactProject_1/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/ReactProject_1/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactProject_1/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13463d2999dcf7d9317",
+    "url": "/ReactProject_1/static/js/main.16b8de6a.chunk.js"
+  },
+  {
+    "revision": "476af1f7fabdda3549e2",
+    "url": "/ReactProject_1/static/js/runtime-main.0f4df18e.js"
+  }
+]);
